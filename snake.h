b98dcbe2bcd58e6697550ec739939a437cddfcc9
devsstudent <example@example.com>
2021-09-28 14:24:42 +0200

    snake.h updated

--- snake.h
+++ snake.h
@@ -1,9 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #define TAILLE_LIGNE 20
+
 #define TAILLE_COLONNE 20
+
 #define HEAD_X 10
+
 #define HEAD_Y 10
+
+int random_pos(void);
+void spawn_fruitz(int map[][TAILLE_LIGNE]);
+
+
 
 
 
