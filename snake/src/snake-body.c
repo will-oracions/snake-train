87d469bef0a50f843a20e316fbcb92c2e1d73045
ahmetUz <pro.ahmetuzn@gmail.com>
2021-10-01 18:07:37 +0200

    change the name of the function -->grown_up to chase_head_and_grown_up

---
+++ snake/src/snake-body.c
@@ -0,0 +1,200 @@
+#include "snake.h"
+/*-----------------------------*/
+
+Snake new_snake(void)
+{
+    return NULL;
+}
+
+/*------------------------------------------------------------------------*/
+
+Snake setup_head(void)
+{
+
+    Snake_Info *head;
+    head = malloc(sizeof(*head));
+    if (head == NULL)
+    {
+        printf("ERROR");
+        exit(EXIT_FAILURE);
+    }
+
+    head->x = HEAD_X;
+    head->y = HEAD_Y;
+
+    head->next = NULL;
+
+    return head;
+}
+
+/*------------------------------------------------------------------------*/
+Snake_M init_snake_mooves(Snake_M m)	
+{
+	m->up = false;
+	m->dawn = false;
+	m->right = false;
+	m->left = false;
+
+	return m;
+}
+	
+
+/*------------------------------------------------------------------------*/
+ Snake moove_head(Snake_M m, Snake s)
+ {
+	 if(m->right == true)
+	 {
+		(s->x)++;
+	 }
+	 if(m->left == true)
+	 {
+		 (s->x)--;
+	 }
+	 if(m->up == true)
+	 {
+		 (s->y)--;
+	 }
+	 if(m->dawn == true)
+	 {
+		 (s->y)++;
+	 }
+
+	 return s;
+ }
+ 
+
+/*------------------------------------------------------------------------*/
+int snake_lenght(Snake s)
+{
+	int len = 0;
+    while (s->next->next != NULL)
+    {
+        s = s->next;
+        len++;
+    }
+    return len;
+}
+
+/*------------------------------------------------------------------------*/
+Snake setup_body(Snake snake_element)
+{
+    Snake_Info *body;
+    body = malloc(sizeof(body));
+    if (body == NULL)
+    {
+       printf("ERROR");
+       exit(EXIT_FAILURE);
+    }
+    body->x = HEAD_X - 1;
+    body->y = HEAD_Y;
+
+    Snake_Info *body1;
+    body1 = malloc(sizeof(*body1));
+    if (body1 == NULL)
+    {
+       printf("ERROR");
+       exit(EXIT_FAILURE);
+    }
+    
+    body1->x = HEAD_X - 2;
+    body1->y = HEAD_Y;
+   
+    body1->next = NULL;
+    
+    body->next = body1;
+    
+    snake_element->next = body;
+
+    return snake_element;
+} 
+
+/*------------------------------------------------------------------------*/
+Snake chase_head(Snake s)
+{
+	Snake_Info *tmp;
+	tmp = s;
+
+	int cmpt = 0;
+	int len = snake_lenght(s);
+
+	int i = len ;
+        int icmpt = 0;
+
+	while(cmpt <  len )
+		
+	{
+		while(i > 0)
+		{
+			tmp = tmp->next;
+			i--;
+		}
+		tmp->next->x = tmp->x;
+		tmp->next->y = tmp->y;
+
+		icmpt++;
+		i = len - icmpt;
+		
+		tmp =s ;
+		cmpt++;
+
+	}
+	tmp->next->x = tmp->x;
+	tmp->next->y = tmp->y;
+
+	return s;	
+}
+/*------------------------------------------------------------------------*/
+
+Snake snake_chase_head_and_grown_up(Snake s, Fruitz fruit)
+{
+	Snake_Info *tmp;
+	tmp = s;
+
+
+	if(s->x == fruit.x && s->y == fruit.y)
+	{
+		Snake_Info *New_Body = malloc(sizeof(New_Body));
+		
+		while(tmp->next != NULL)
+		{
+			tmp = tmp->next;
+		}
+		New_Body->next = NULL;
+		tmp->next = New_Body;
+
+	}
+
+	s = chase_head(s); 
+
+	return s;
+}
+/*------------------------------------------------------------------------*/
+
+void check_snake(Snake snak)
+{
+    if(snak == NULL)
+        printf("list empty");
+    while (snak != NULL)
+    {
+        printf("%d",snak->y);
+        snak = snak->next;
+    }
+}
+
+
+/*------------------------------------------------------------------------*/
+	
+/*------------------------------------------------------------------------*/
+
+/*-----------------------------*/
+
+/*-----------------------------*/
+
+/*------------------------------------------------------------------------*/
+	
+/*------------------------------------------------------------------------*/
+
+/*------------------------------------------------------------------------*/
+
+/*------------------------------------------------------------------------*/
+
---
+++ snake/src/snake.h
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <SDL2/SDL.h>
+
+//gcc src/*.c -o bin/prog -I include -L lib -l SDL2
+#ifndef __SNAKE__H__
+#define __SNAKE__H__
+
+	#define TAILLE_LIGNE 20
+
+	#define TAILLE_COLONNE 20
+
+	#define HEAD_X 10
+	
+	#define HEAD_Y 10
+	
+	/*-----------------------------*/
+	typedef enum
+	{
+		false,
+		true
+	}Bool;
+	
+	/*-----------------------------*/
+
+	typedef struct Snake_Mooves
+	{
+		Bool up; // = 1
+		Bool dawn;// = 2
+		Bool right;// = 3
+		Bool left;// = 4
+
+	}Snake_Mooves, *Snake_M;
+	
+	
+	/*-----------------------------*/
+	typedef struct Snake_Info
+	{
+		int x;
+		int y;
+	
+		struct Snake_Info *next;
+	
+	}Snake_Info, *Snake;
+	/*-----------------------------*/
+	
+	/*-----------------------------*/
+
+	typedef struct Fruits Fruitz;
+	struct Fruits
+        {
+        	int x;
+		int y;
+	};
+
+	/*----------------------------Prototype fruitz.c--------------------------*/
+	int random_pos(void);
+	Fruitz spawn_fruitz(Fruitz fruit, int map[][TAILLE_LIGNE]);
+	/*------------------------------------------------------------------------*/
+	
+	
+	/*--------------------------Prototype map.c-------------------------------*/
+   
+	void init_map(int map[][TAILLE_COLONNE]);
+	void display_map(int map[][TAILLE_COLONNE]);
+	Bool check_snake_pos(Snake snake, int map[][TAILLE_COLONNE]);
+	void insert_snake(int map[][TAILLE_COLONNE], Snake snake_element);
+	/*------------------------------------------------------------------------*/
+	
+	/*------------------------Prototype snake-body.c--------------------------*/
+	Snake new_snake(void);
+	Bool verif_snake_info_empty(Snake s);
+	Snake_M init_snake_mooves(Snake_M m);	
+	Snake init_snake(Snake s);
+        
+	int snake_lenght(Snake s);
+	Snake moove_head(Snake_M m, Snake s);  
+	Snake chase_head(Snake s);
+
+	Snake snake_chase_head_and_grown_up(Snake s, Fruitz fruit);
+	Snake setup_head(void);
+	Snake setup_body(Snake snake_element);
+	void check_snake(Snake snak);
+	
+	
+	/*------------------------------------------------------------------------*/
+#endif
